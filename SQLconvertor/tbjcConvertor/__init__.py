b7899d987195559433566e73aa86007be2d316cd
tbjc1magic <tbjc1magic@gmail.com>
2018-10-27 04:04:04 +0000

    add exception handle to remove temporary file

--- SQLconvertor/tbjcConvertor/__init__.py
+++ SQLconvertor/tbjcConvertor/__init__.py
@@ -7,7 +7,7 @@ def convert(fin, fout, ftmp='tmp.db'):
     try:
         Convertor_lite.process(fin, ftmp)
         Convert_sql2hdf.process(ftmp, fout)
-    except:
+    finally:
         os.remove(ftmp)
 
 
