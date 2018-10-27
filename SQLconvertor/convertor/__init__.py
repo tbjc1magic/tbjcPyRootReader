5528cfbe357a71fe958c9dff4d0c267390237c6f
tbjc1magic <tbjc1magic@gmail.com>
2018-10-27 03:48:27 +0000

    add exception message

--- SQLconvertor/convertor/__init__.py
+++ SQLconvertor/convertor/__init__.py
@@ -2,7 +2,8 @@ from . import Convert_sql2hdf
 from . import Convertor_lite  
 import os.path
 def convert(fin, fout, ftmp='tmp.db'):
-    if not os.path.isfile(fin): return 
+    if not os.path.isfile(fin):
+        raise Exception("file does not exit")
     Convertor_lite.process(fin, ftmp)
     Convert_sql2hdf.process(ftmp, fout)
 
